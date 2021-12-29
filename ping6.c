cb5f0495a7087fc4d15b0859d245bec67e1054ce
Chul-Woong Yang <cwyang@gmail.com>
2021-12-29 16:35:29 +0900

    remove in_cksum

--- ping6.c
+++ ping6.c
@@ -48,39 +48,6 @@ char * pr_addr(void *sa, socklen_t salen)
     return(buffer);
 }
 
-
-unsigned short
-in_cksum(const unsigned short *addr, register int len, unsigned short csum)
-{
-    register int nleft = len;
-    const unsigned short *w = addr;
-    register unsigned short answer;
-    register int sum = csum;
-
-    /*
-     *  Our algorithm is simple, using a 32 bit accumulator (sum),
-     *  we add sequential 16 bit words to it, and at the end, fold
-     *  back all the carry bits from the top 16 bits into the lower
-     *  16 bits.
-     */
-    while (nleft > 1)  {
-        sum += *w++;
-        nleft -= 2;
-    }
-
-    /* mop up an odd byte, if necessary */
-    if (nleft == 1)
-        sum += ODDBYTE(*(unsigned char *)w); /* le16toh() may be unavailable on old systems */
-
-    /*
-     * add back carry outs from top 16 bits to low 16 bits
-     */
-    sum = (sum >> 16) + (sum & 0xffff);	/* add hi 16 to low 16 */
-    sum += (sum >> 16);			/* add carry */
-    answer = ~sum;				/* truncate to 16 bits */
-    return (answer);
-}
-
 /*
  * pinger --
  *      Compose and transmit an ICMP ECHO REQUEST packet.  The IP packet
